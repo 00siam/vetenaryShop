33979fcbe2c9ac7368e5dfdf3e9d3947506a21e9
Siam <baust.cse.200201018@gmail.com>
2023-03-02 15:34:31 +0600

    crosal top fixed

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
       <Nav />
 
       <div>
-        <Carousel fade>
+        <Carousel fade style={{marginTop:55}}>
           <Carousel.Item>
             <img
               className="d-block w-100"
